0c19be74f74c1c34cfad507f992e7bd2c6ec9dae
Yacine Belarbi <yacbel@gmail.com>
2015-12-07 09:39:49 +0100

    url redirect page for instagram

---
+++ france-hossegor/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hossegor, France</title>
+</head>
+<body>
+<?php
+header('Location: '."https://www.youtube.com/watch?v=Dfx12YJOTEs");
+?>
+</body>
+</html>
